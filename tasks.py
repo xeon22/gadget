edab998bf2205154d61ddc223abcf6de9e929de3
Mark Pimentel <mark.pimentel@capco.com>
2020-09-08 17:11:11 -0400

    Added tasks file for running locally

--- tasks.py
+++ tasks.py
@@ -1,19 +1,25 @@
 
+import logging
 from invoke import Collection
+from rich.logging import RichHandler
+# import gadget.tasks
 
-from pz import (
+from gadget.tasks import (
     init,
-    azure,
-    bitbucket,
-    jira,
-    servicedesk,
     artifactory,
-    utils,
+    servicedesk,
+    azure,
     tls,
-    kubernetes
+    jira,
+    kubernetes,
+    utils,
+    bitbucket
 )
 
+
 ns = Collection()
+# ns.from_module(gadget)
+
 ns.add_collection(init)
 ns.add_collection(bitbucket)
 ns.add_collection(jira)
@@ -24,3 +30,9 @@ ns.add_collection(tls)
 ns.add_collection(kubernetes)
 ns.add_collection(azure)
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(module).19s:%(lineno)s] %(message)s',
+    datefmt="[%X]",
+    handlers=[RichHandler()]
+)
