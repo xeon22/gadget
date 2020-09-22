b4f9a31c9ccfe7aeec3cffbedcec10c838c877d2
Mark Pimentel <mark.pimentel@capco.com>
2020-09-22 17:02:21 -0400

    Cleaned up utils

--- gadget/tasks/utils.py
+++ gadget/tasks/utils.py
@@ -4,22 +4,18 @@ import json
 import yaml
 
 from invoke import task, config
-from rich.logging import RichHandler
+
 
 def objectify(hash):
     return types.SimpleNamespace(**hash)
 
-# def init_logging():
-#     logging.basicConfig(
-#         level=logging.INFO,
-#         format='[%(module).19s:%(lineno)s] %(message)s',
-#         datefmt="[%X]",
-#         handlers=[RichHandler()]
-#     )
-#
-#     return logging.getLogger("rich")
 
-@task
+def print_json(data):
+    # jdata = json.loads(data)
+    return json.dumps(data, indent=2, sort_keys=True)
+
+
+@task()
 def gen_client_name(ctx):
     alpha = random.choices(string.ascii_letters, k=1)
     num = random.choices(string.digits, k=3)
@@ -27,11 +23,6 @@ def gen_client_name(ctx):
     id = ''.join(alpha + num).lower()
 
     logging.info(id)
-
-
-def print_json(data):
-    # jdata = json.loads(data)
-    return json.dumps(data, indent=2, sort_keys=True)
 
 
 @task()
