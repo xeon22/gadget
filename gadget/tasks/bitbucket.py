2c04da2039438fbf97aac0914da7d1a1d489c680
Mark Pimentel <mark.pimentel@capco.com>
2021-02-01 14:00:53 -0500

    Added bitbucket user audit report

--- gadget/tasks/bitbucket.py
+++ gadget/tasks/bitbucket.py
@@ -5,8 +5,14 @@ from invoke import task
 from atlassian import Bitbucket
 from rich.console import Console
 from rich.table import Table
+from urllib.parse import urlparse
+from contextlib import closing
 import requests
 import logging
+import json
+import sqlite3
+import os
+import re
 
 console = Console()
 
@@ -65,6 +71,56 @@ def get_repo(ctx, repo):
         console.print(data)
     except requests.exceptions.HTTPError as e:
         logging.error(e)
+
+
+@task(pre=[init])
+def get_all_repos(ctx, workspace):
+    # workspace, _repo = repo_check(repo)
+    url_path = f"/2.0/repositories/{workspace}?sort=name"
+    results = list()
+
+    try:
+        data = ctx.config.main.bitbucket.client.get(path=url_path)
+        results.extend(data['values'])
+
+        if 'next' in data.keys():
+            has_next = True
+
+        while has_next:
+            u = urlparse(data['next'])
+            logging.info(f"Fetching results page: {data['page']}")
+
+            url_path = f"{u.path}?{u.query}"
+            data = ctx.config.main.bitbucket.client.get(path=url_path)
+            results.extend(data['values'])
+
+            has_next = data.get('next')
+
+        # console.print(data)
+    except requests.exceptions.HTTPError as e:
+        logging.error(e)
+
+    table = Table(
+        "Name",
+        "Project",
+        "Language",
+        "Created",
+        title=f"Repositories({data['size']})"
+    )
+
+    for item in results:
+        create_time = datetime.fromisoformat(item['created_on'])
+        from datetime import timezone
+
+        table.add_row(
+            item['name'],
+            item['project']['key'],
+            item['language'],
+            datetime.strftime(create_time, '%b %m %Y'),
+        )
+
+    console.print(table)
+
 
 
 @task(pre=[init])
@@ -278,10 +334,247 @@ def get_branches(ctx, repo):
 
 @task(pre=[init])
 def get_projects(ctx, workspace):
-    projects = ctx.config.main.bitbucket.client.put(f"/2.0/workspaces/{workspace}/projects")
+    # projects = ctx.config.main.bitbucket.client.put(f"/2.0/workspaces")
+    projects = ctx.config.main.bitbucket.client.repo_list('capcosaas', limit=1000)
 
     console.print(projects)
+    console.print(dir(projects))
 
-    for project in projects:
-        console.print(project)
+    breakpoint()
+
+    # for project in projects:
+    #     console.print(project)
+
+
+@task(pre=[init])
+def get_users(ctx, workspace):
+    data = ctx.config.main.bitbucket.client.get(f"/2.0/workspaces/{workspace}/permissions")
+    console.print(data['values'][0])
+
+    try:
+        results = data['values']
+
+        if 'next' in data.keys():
+            has_next = True
+
+        while has_next:
+            u = urlparse(data['next'])
+            logging.info(f"Fetching results page: {data['page']}")
+
+            url_path = f"{u.path}?{u.query}"
+            data = ctx.config.main.bitbucket.client.get(path=url_path)
+            results.extend(data['values'])
+
+            has_next = data.get('next')
+
+        # console.print(data)
+    except requests.exceptions.HTTPError as e:
+        logging.error(e)
+
+    table = Table(
+        "DisplayName",
+        "NickName",
+        "Permission",
+        "Created",
+        title=f"Repositories({data['size']})"
+    )
+
+    for item in sorted(results, key=lambda i: i['user']['display_name']):
+        # create_time = datetime.fromisoformat(item['added_on'])
+        from datetime import timezone
+
+        table.add_row(
+            item['user']['display_name'],
+            item['user']['nickname'],
+            item['permission'],
+            # datetime.strftime(create_time, '%b %m %Y'),
+        )
+
+    console.print(table)
+
+
+def init_db(ctx, db="bitbucket.db"):
+    schema = [
+        """
+        CREATE TABLE user (
+            uuid TEXT,
+            display_name TEXT,
+            nickname TEXT,
+            account_id TEXT
+        );
+        """,
+
+        """
+        CREATE TABLE repo (
+            uuid TEXT,
+            name TEXT,
+            full_name TEXT
+        );
+        """,
+
+        """
+        CREATE TABLE permission (
+            user_id TEXT,
+            repo_id TEXT,
+            permission TEXT
+        );
+        """
+    ]
+
+    if not os.path.exists("bitbucket.db"):
+        connection = sqlite3.connect(db)
+        cursor = connection.cursor()
+        for q in schema:
+            cursor.execute(q)
+
+        connection.commit()
+        connection.close()
+
+
+def insert_user(conn, data):
+    """
+    Create a new task
+    :param conn:
+    :param task:
+    :return:
+    """
+
+    sql = """
+        INSERT INTO user(uuid, display_name, nickname, account_id)
+        VALUES(?,?,??)
+    """
+
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit()
+
+    return cur.lastrowid
+
+
+@task(pre=[init])
+def get_permissions(ctx, workspace, table=False):
+    data = ctx.config.main.bitbucket.client.get(f"/2.0/workspaces/{workspace}/permissions/repositories?sort=user.nickname")
+    console.print(data['values'][0])
+
+    try:
+        results = data['values']
+
+        if 'next' in data.keys():
+            has_next = True
+
+        while has_next:
+            u = urlparse(data['next'])
+            logging.info(f"Fetching results page: {data['page']}")
+
+            url_path = f"{u.path}?{u.query}"
+            data = ctx.config.main.bitbucket.client.get(path=url_path)
+            results.extend(data['values'])
+
+            has_next = data.get('next')
+
+        # console.print(data)
+    except requests.exceptions.HTTPError as e:
+        logging.error(e)
+
+    with open('results.json', 'w') as fh:
+        fh.write(json.dumps(results, indent=2))
+
+    table = Table(
+        "DisplayName",
+        "NickName",
+        "Repository",
+        "Permission",
+        title=f"Permissions({data['size']})"
+    )
+
+    # uuid = re.sub(r'{|}', '', data['user']['uuid'])
+
+    for item in results:
+        for item in sorted(results, key=lambda i: i['user']['display_name']):
+        # create_time = datetime.fromisoformat(item['added_on'])
+        # from datetime import timezone
+
+            if table:
+                table.add_row(
+                    item['user']['display_name'],
+                    item['user']['nickname'],
+                    item['repository']['full_name'],
+                    item['permission'],
+                    # datetime.strftime(create_time, '%b %m %Y'),
+                )
+
+    # console.print(table)
+
+
+@task()
+def load_permissions(ctx, input="results.json", db="bitbucket.db"):
+    init_db(ctx)
+
+    raw = json.load(open(input, 'r'))
+    logging.info(f"Loaded input file {input} with {len(raw)} records")
+
+    with open('bitbucket_report.csv', 'w') as fh:
+        fh.write("Name,Repo,Permission\n")
+        current_user = ""
+
+        for item in raw:
+            if current_user == item['user']['uuid']:
+                data = [
+                    "",
+                    item['repository']['full_name'],
+                    item['permission']
+                ]
+            else:
+                data = [
+                    item['user']['display_name'],
+                    item['repository']['full_name'],
+                    item['permission']
+                ]
+
+            fh.write(f"{','.join(data)}\n")
+            current_user = item['user']['uuid']
+
+    with closing(sqlite3.connect(db)) as connection:
+        with closing(connection.cursor()) as cursor:
+            for item in raw:
+
+                # user_uuid = re.sub(r'{|}', '', item['user']['uuid'])
+
+                user = f"""
+                    INSERT INTO 
+                        user (uuid, display_name, nickname, account_id)
+                    VALUES(
+                        "{item['user']['uuid']}",
+                        "{item['user']['display_name']}",
+                        "{item['user']['nickname']}",
+                        "{item['user']['account_id']}"
+                    )
+                """
+                cursor.execute(user)
+
+                repo = f"""
+                    INSERT INTO 
+                        repo (uuid, name, full_name)
+                    VALUES(
+                        "{item['repository']['uuid']}",
+                        "{item['repository']['name']}",
+                        "{item['repository']['full_name']}"
+                    )
+                """
+                cursor.execute(repo)
+
+                perm = f"""
+                    INSERT INTO 
+                        permission (user_id, repo_id, permission)
+                    VALUES(
+                        "{item['user']['uuid']}",
+                        "{item['repository']['uuid']}",
+                        "{item['permission']}"
+                    )
+                """
+                cursor.execute(perm)
+
+
+
+        connection.commit()
 
