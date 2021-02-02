e626a217c19ed5e9d65ad9bf2e9288d28030b1d0
Mark Pimentel <mark.pimentel@capco.com>
2021-02-01 23:18:07 -0500

    Updated digicert tasks for cert renewal

--- gadget/tasks/digicert.py
+++ gadget/tasks/digicert.py
@@ -1,4 +1,4 @@
-
+import base64
 from gadget.tasks import init, utils, confluence
 from invoke import task, Collection, Executor
 from invoke.main import program
@@ -23,7 +23,8 @@ class OrdersResource(Resource):
 
 class CertResource(Resource):
     actions = {
-        'download': {'method': 'GET', 'url': '/certificate/download/order/{}/format/{}'}
+        'download': {'method': 'GET', 'url': '/certificate/download/order/{}/format/{}'},
+        'renew': {'method': 'POST', 'url': '/order/certificate/ssl_basic'}
     }
 
 
@@ -54,15 +55,14 @@ def init(ctx, headers=dict(), params=dict()):
 @task(pre=[init])
 def list_orders(ctx):
     api = ctx.config.main.digicert.api
-    orders = api.orders.list(body=None)
+    orders = api.orders.list(body=None, params={'filters[status]': 'issued'})
 
-    console.print(orders.body)
     columns = [
         "OrderId",
         "CommonName",
-        "ValidFrom",
-        "ValidUntil",
-        "DaysLeft"
+        "Valid From",
+        "Valid Until",
+        "Days Left",
     ]
 
     rows = []
@@ -70,8 +70,10 @@ def list_orders(ctx):
     table = Table(*columns, title="Certificate Catalog")
     orders_list = orders.body.get('orders')
 
-    for item in orders.body.get('orders'):
-        if item['status'] == 'rejected':
+    for item in orders_list:
+        console.print(item)
+
+        if item['status'] != 'issued':
             continue
 
         row = [
@@ -79,7 +81,7 @@ def list_orders(ctx):
             item['certificate']['common_name'],
             item['certificate']['valid_from'],
             item['certificate']['valid_till'],
-            str(item['certificate']['days_remaining'])
+            str(item['certificate']['days_remaining']),
         ]
 
         table.add_row(*row)
@@ -118,30 +120,11 @@ def list_orders(ctx):
         content=output
     )
 
-    # res = Executor(Collection(confluence), config=ctx.config, core=program.core).execute(
-    #     [
-    #         ("publish_page", {"template": template, "page_id": ctx.config.main.digicert.confluence_page, "columns": columns})
-    #     ]
-    # )
-
 
 @task(pre=[init])
 def order(ctx, id):
     api = ctx.config.main.digicert.api
     order = api.orders.info(id).body
-
-    console.log(order)
-
-    table = Table(
-        "OrderId",
-        "CommonName",
-        "ValidFrom",
-        "ValidUntil",
-        "Created",
-        "DnsNames",
-        "Product",
-        title="Orders",
-    )
 
     grid = Table.grid()
 
@@ -152,21 +135,31 @@ def order(ctx, id):
     grid.add_row("Valid Until", order['certificate']['valid_till'])
     grid.add_row("Created", order['certificate']['date_created'])
     grid.add_row("Dns Names", str(order['certificate']['dns_names']))
-    grid.add_row("Product", str(order['product']))
+    grid.add_row("Product", order['product']['name'])
     grid.add_row("Key Size", str(order['certificate']['key_size']))
-    grid.add_row("Organization", str(order['organization']))
-    grid.add_row("Product", str(order['product']))
+    grid.add_row(
+        "Organization",
+        "\n".join(
+            [
+                order['organization']['name'],
+                order['organization']['assumed_name'],
+                order['organization']['display_name'],
+                order['organization']['city'],
+                order['organization']['state'],
+                order['organization']['country'].upper(),
+                order['organization']['telephone'],
+            ]
+        )
+    )
 
-
-    console.log(grid)
-    # console.log(dir(grid))
+    console.print(grid)
 
 
 @task(pre=[init])
 def download(ctx, id, format="pem_nointermediate", output=None):
     api = ctx.config.main.digicert.api
     cert = api.cert.download(id, format).body
-    print(cert.decode('utf-8'))
+    console.print(cert.decode('utf-8'))
 
     if output:
         with open(output, 'w') as f:
@@ -174,14 +167,66 @@ def download(ctx, id, format="pem_nointermediate", output=None):
 
 
 @task(pre=[init])
-def renew(ctx):
-    from azure.identity import DefaultAzureCredential
+def renew(ctx, order, vault, keyName, csrName=None, certName=None):
+    from azure.identity import AzureCliCredential, DefaultAzureCredential
     from azure.keyvault.secrets import SecretClient
 
-    credential = DefaultAzureCredential()
+    import OpenSSL.crypto
+    from OpenSSL.crypto import load_certificate_request, FILETYPE_PEM
 
-    secret_client = SecretClient(vault_url="https://my-key-vault.vault.azure.net/", credential=credential)
-    secret = secret_client.get_secret("secret-name")
+    api = ctx.config.main.digicert.api
+    order_data = api.orders.info(order).body
 
-    print(secret.name)
-    print(secret.value)
+    console.log(order_data)
+
+    if csrName is None:
+        csrName = keyName.replace('-key', '-csr')
+
+    if certName is None:
+        certName = keyName.replace('-key', '-cert')
+
+    try:
+        credential = AzureCliCredential()
+    except Exception as e:
+        credential = DefaultAzureCredential()
+
+    vault_client = SecretClient(vault_url=f"https://{vault}.vault.azure.net/", credential=credential)
+    
+    vault_key_name = vault_client.get_secret(keyName)
+    csr_key_name = vault_client.get_secret(csrName)
+
+    tls_private_key = base64.b64decode(vault_key_name.value).decode('utf-8')
+    tls_order_csr = base64.b64decode(csr_key_name.value).decode('utf-8')
+
+    print(tls_private_key)
+    print(tls_order_csr)
+
+    console.log("Loading the CSR")
+    req = load_certificate_request(FILETYPE_PEM, tls_order_csr)
+    subject = dict(req.get_subject().get_components())
+
+    payload = {
+        'certificate': {
+            'common_name': order_data['certificate']['common_name'],
+            'dns_names': order_data['certificate']['dns_names'],
+            'csr': tls_order_csr,
+            'signature_hash': order_data['certificate']['signature_hash'],
+            'server_platform': {'id': order_data['certificate']['server_platform']['id']},
+        },
+        'container': order_data['container']['id'],
+        'auto_renew': 0,
+        'organization': {'id': order_data['organization']['id']},
+        'order_validity': order_data['validity_years'],
+        'validity_years': order_data['validity_years'],
+        'payment_method': 'card',
+        'additional_emails': [
+            'mark.pimentel@capco.com',
+            'catodevopsteam@capco.com',
+        ]
+    }
+
+    console.log(payload)
+
+    # order = api.orders.renew
+
+    # print(response.body)
